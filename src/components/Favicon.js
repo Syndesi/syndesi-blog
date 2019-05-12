ff5bf5bd6bc2ec555fbe88e71c8b9830fecd2c30
Syndesi <soerenklein98@gmail.com>
2019-05-12 15:49:02 +0200

    Changed favicons from dynamic to static versions - most browsers still look under /favicon.ico

--- src/components/Favicon.js
+++ src/components/Favicon.js
@@ -7,14 +7,15 @@ export default class Favicon extends React.Component {
     // files generated with realfavicongenerator.net
     return (
       <Helmet>
-        <link rel="shortcut icon" href={require('../assets/favicon/favicon.ico')} type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="180x180" href={require('../assets/favicon/apple-touch-icon.png')} />
-        <link rel="icon" type="image/png" sizes="32x32" href={require('../assets/favicon/favicon-32x32.png')} />
-        <link rel="icon" type="image/png" sizes="16x16" href={require('../assets/favicon/favicon-16x16.png')} />
-        <link rel="manifest" href={require('../assets/favicon/site.webmanifest')} />
-        <link rel="mask-icon" href={require('../assets/favicon/safari-pinned-tab.svg')} color="#282828" />
-        <meta name="msapplication-TileColor" content="#2d89ef" />
-        <meta name="msapplication-TileImage" content={require('../assets/favicon/mstile-144x144.png')} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d1d1b" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
       </Helmet>
     );
