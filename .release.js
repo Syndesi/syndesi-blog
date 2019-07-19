5911d7699ea39db5e75207f97ad484edbd21510e
Syndesi <soerenklein98@gmail.com>
2019-07-19 20:32:28 +0200

    release-building test 6

--- .release.js
+++ .release.js
@@ -2,16 +2,26 @@ const { zip } = require('zip-a-folder');
 const fs = require('fs');
 
 
-class ZipAFolder {
+class Release {
 
   static async main() {
     let version = process.argv[2] || 'unknown_version';
-    let outputPath = './release/'+version;
+    let outputPath = './release/';
     if (!fs.existsSync(outputPath)){
       fs.mkdirSync(outputPath, { recursive: true });
+    } else {
+      // clear folder
+      fs.readdir(outputPath, (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+          fs.unlink(path.join(directory, file), err => {
+            if (err) throw err;
+          });
+        }
+      });
     }
     await zip('./dist', outputPath + '/dist_'+version+'.zip');
   }
 }
 
-ZipAFolder.main();
+Release.main();
